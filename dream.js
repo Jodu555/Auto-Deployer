a755fcced9be10eb8c49c653a4d6f76319c5d4d7
Jodu555 <Jodu505@gmail.com>
2022-01-19 22:32:01 +0100

    Get the step things working + added the createDeployment dir

--- dream.js
+++ dream.js
@@ -13,9 +13,19 @@ class Deploy {
     constructor(ID, steps) {
         this.ID = ID;
         this.steps = steps;
+        this.stepIdx = 0;
+        this.dir = null;
+        this.currStep = null;
+    }
+
+    currentStep() {
+        console.log(123);
+        return this.currStep ? this.currStep : this.steps[this.stepIdx];
     }
 
     createDeploy() {
+        this.dir = path.join(deploymentsDirectory, `#${this.ID} - Deployment`);
+        fs.mkdirSync(this.dir);
     }
     deleteDeploy() {
 
@@ -23,8 +33,13 @@ class Deploy {
     exec() {
 
     }
-    step() {
-
+    step(name) {
+        if (name) {
+            this.currStep = name
+        } else {
+            this.stepIdx++;
+            this.currStep = null;
+        }
     }
     delete() {
 
@@ -43,21 +58,32 @@ class Host {
     }
 }
 
-registerDeploy('project-name-html', ['Download', 'Deletion', 'Upload'], (deploy, host) => {
-    deploy.createDeploy(); // Creates a dir to do the deploy in
-    deploy.exec('git pull origin master'); // Executes any ssh-deploy command in the dir
-    deploy.step(); // Steps to the next in this case 'Deletion'
-    deploy.delete('README.md'); // Deletes on file in the dir
-    deploy.delete(['.git', '.gitignore']); // Deletes an array of files in the dir
-    deploy.step('Build'); // Steps to Building
-    deploy.exec('npm run build')
-    deploy.step(); //Steps to Upload
-    host.connect('IP-DEPLOY-SERVER', 'username', 'password', 'PATH-TO-DIR'); // Connects to the deploy host and decides the path to upload in this case: /var/ww/html/proj
-    host.upload(); // This will upload everything left in the dir
-    host.upload('PATH');// This will upload everything in the path
-    host.disconnect(); // Cleanes the connection
-    deploy.deleteDeploy(); // Deletes the dir where the deploy was done
-});
+const deploy = new Deploy('Test-123', ['Download', 'Test', 'Deletion', 'Upload']);
+console.log(deploy.currentStep());
+deploy.step();
+console.log(deploy.currentStep());
+deploy.step();
+console.log(deploy.currentStep());
+deploy.step('Build');
+console.log(deploy.currentStep());
+deploy.step();
+console.log(deploy.currentStep());
+
+// registerDeploy('project-name-html', ['Download', 'Deletion', 'Upload'], (deploy, host) => {
+//     deploy.createDeploy(); // Creates a dir to do the deploy in
+//     deploy.exec('git pull origin master'); // Executes any ssh-deploy command in the dir
+//     deploy.step(); // Steps to the next in this case 'Deletion'
+//     deploy.delete('README.md'); // Deletes on file in the dir
+//     deploy.delete(['.git', '.gitignore']); // Deletes an array of files in the dir
+//     deploy.step('Build'); // Steps to Building
+//     deploy.exec('npm run build')
+//     deploy.step(); //Steps to Upload
+//     host.connect('IP-DEPLOY-SERVER', 'username', 'password', 'PATH-TO-DIR'); // Connects to the deploy host and decides the path to upload in this case: /var/ww/html/proj
+//     host.upload(); // This will upload everything left in the dir
+//     host.upload('PATH');// This will upload everything in the path
+//     host.disconnect(); // Cleanes the connection
+//     deploy.deleteDeploy(); // Deletes the dir where the deploy was done
+// });
 
 function registerDeploy(name, steps, cb) {
     console.log(name, steps, cb(new Deploy(name, steps), new Host()));

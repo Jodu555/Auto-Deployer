7cf8e553d54bd9a7c7e6fe0197ebc457fbe4e46c
Jodu555 <Jodu505@gmail.com>
2022-01-19 22:16:44 +0100

    Renamed the ssh to deploy

--- dream.js
+++ dream.js
@@ -5,7 +5,7 @@
  *      Of what i would like this to be
  */
 
-class SSH {
+class Deploy {
     constructor(ID, steps) {
         this.ID = ID;
         this.steps = steps;
@@ -40,23 +40,23 @@ class Host {
     }
 }
 
-registerDepoy('project-name-html', ['Download', 'Deletion', 'Upload'], (ssh, host) => {
-    ssh.createDeploy(); // Creates a dir to do the deploy in
-    ssh.exec('git pull origin master'); // Executes any ssh command in the dir
-    ssh.step(); // Steps to the next in this case 'Deletion'
-    ssh.delete('README.md'); // Deletes on file in the dir
-    ssh.delete(['.git', '.gitignore']); // Deletes an array of files in the dir
-    ssh.step('Build'); // Steps to Building
-    ssh.exec('npm run build')
-    ssh.step(); //Steps to Upload
+registerDepoy('project-name-html', ['Download', 'Deletion', 'Upload'], (deploy, host) => {
+    deploy.createDeploy(); // Creates a dir to do the deploy in
+    deploy.exec('git pull origin master'); // Executes any ssh-deploy command in the dir
+    deploy.step(); // Steps to the next in this case 'Deletion'
+    deploy.delete('README.md'); // Deletes on file in the dir
+    deploy.delete(['.git', '.gitignore']); // Deletes an array of files in the dir
+    deploy.step('Build'); // Steps to Building
+    deploy.exec('npm run build')
+    deploy.step(); //Steps to Upload
     host.connect('IP-DEPLOY-SERVER', 'username', 'password', 'PATH-TO-DIR'); // Connects to the deploy host and decides the path to upload in this case: /var/ww/html/proj
     host.upload(); // This will upload everything left in the dir
     host.upload('PATH');// This will upload everything in the path
     host.disconnect(); // Cleanes the connection
-    ssh.deleteDeploy(); // Deletes the dir where the deploy was done
+    deploy.deleteDeploy(); // Deletes the dir where the deploy was done
 });
 
 function registerDepoy(name, steps, cb) {
-    console.log(name, steps, cb(new SSH(name, steps)));
+    console.log(name, steps, cb(new Deploy(name, steps)));
 }
 

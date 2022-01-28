7aeba7abee17a59ec46ba34a358a4763ea2f35bb
Jodu555 <Jodu505@gmail.com>
2022-01-28 14:47:57 +0100

    Moved the host class also its own file

--- dream.js
+++ dream.js
@@ -4,49 +4,7 @@ const dotenv = require('dotenv').config();
 
 const Deploy = require('./src/classes/Deploy');
 const Config = require('./src/classes/Config');
-class Host {
-    constructor(deploy) {
-        this.deploy = deploy;
-        this.cwd = '';
-        this.ssh = new NodeSSH();
-    }
-    async connect(host, username, password, initPath) {
-        const config = {};
-        if (host.host) {
-            config = host
-            initPath = username;
-        } else {
-            config = { host, username, password }
-        }
-        await this.ssh.connect(config);
-        this.cwd = initPath;
-    }
-    async upload(uploadPath = null) {
-        const failed = []
-        const succeeded = []
-        const success = await this.ssh.putDirectory(this.deploy.dir, uploadPath ? path.join(this.cwd, uploadPath) : this.cwd, {
-            recursive: true,
-            concurrency: 10,
-            tick: (localPath, remotePath, error) => {
-                const arr = this.deploy.getCurrentRecord()?.upload?.part || [];
-                arr.push({ localPath, remotePath, error });
-                this.deploy.appendRecord({ upload: { part: arr } });
-                if (error) {
-                    failed.push(localPath)
-                } else {
-                    succeeded.push(localPath)
-                }
-            }
-        });
-        this.deploy.appendRecord({ upload: { success, failed, succeeded } });
-    }
-    async exec() {
-        //Important: This method executes an ssh command direct on your Host Machine
-    }
-    disconnect() {
-        this.ssh.dispose();
-    }
-}
+
 
 registerDeploy('Personal-Website', ['Download', 'Deletion', 'Upload'], async (deploy, host, data, config) => {
     deploy.createDeploy();
---
+++ src/classes/Host.js
@@ -0,0 +1,43 @@
+class Host {
+    constructor(deploy) {
+        this.deploy = deploy;
+        this.cwd = '';
+        this.ssh = new NodeSSH();
+    }
+    async connect(host, username, password, initPath) {
+        const config = {};
+        if (host.host) {
+            config = host
+            initPath = username;
+        } else {
+            config = { host, username, password }
+        }
+        await this.ssh.connect(config);
+        this.cwd = initPath;
+    }
+    async upload(uploadPath = null) {
+        const failed = []
+        const succeeded = []
+        const success = await this.ssh.putDirectory(this.deploy.dir, uploadPath ? path.join(this.cwd, uploadPath) : this.cwd, {
+            recursive: true,
+            concurrency: 10,
+            tick: (localPath, remotePath, error) => {
+                const arr = this.deploy.getCurrentRecord()?.upload?.part || [];
+                arr.push({ localPath, remotePath, error });
+                this.deploy.appendRecord({ upload: { part: arr } });
+                if (error) {
+                    failed.push(localPath)
+                } else {
+                    succeeded.push(localPath)
+                }
+            }
+        });
+        this.deploy.appendRecord({ upload: { success, failed, succeeded } });
+    }
+    async exec() {
+        //Important: This method executes an ssh command direct on your Host Machine
+    }
+    disconnect() {
+        this.ssh.dispose();
+    }
+}

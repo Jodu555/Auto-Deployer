f2f19dc7fd6a3969486f68c38a1cd14f4c00c89f
Jodu555 <Jodu505@gmail.com>
2022-02-01 08:33:37 +0100

    Wrote an own simultaneously upload script which actually is way way better then the given...

--- speedtest.js
+++ speedtest.js
@@ -3,17 +3,56 @@ const path = require('path')
 const { NodeSSH } = require('node-ssh')
 
 const ssh = new NodeSSH()
-
-ssh.connect({
-
-});
-
 // Putting entire directories
-const p = path.join(__dirname, 'deployments', '#Personal-Website - Deployment') + '\\';
+const p = path.join(__dirname, 'deployments', '#Personal-Website - Deployment');
 console.log(p);
-// return;
-up();
-function up() {
+
+
+function uploadSingles(lcPath, rmPath) {
+    const files = [];
+    fs.readdirSync(lcPath).map(e => { return { name: e, path: path.join(lcPath, e) } }).forEach(entity => {
+        if (fs.statSync(entity.path).isDirectory()) {
+            files.push(...uploadSingles(path.join(entity.path), path.join(rmPath, entity.name)));
+        } else {
+            files.push({ lc: entity.path, rm: path.join(rmPath, entity.name) });
+        }
+    });
+    return files;
+}
+
+// console.log(uploadSingles(p, '/home/TEST'));
+
+test(uploadSingles(p, '/home/TEST'));
+
+async function test(files) {
+    await ssh.connect({
+        host: 'ci.jodu555.de',
+        username: 'root',
+        password: 'xxxx'
+    });
+
+    const uploadProcesses = [];
+
+    // files.forEach(entity => {
+    //     uploadProcesses.push(ssh.putFiles([{ local: entity.lc, remote: entity.rm }]).then(function () {
+    //         console.log(entity, "The File thing is done")
+    //     }, function (error) {
+    //         console.log(entity, "Something's wrong")
+    //         console.log(error)
+    //     }));
+    // });
+    for (const entity of files) {
+        await ssh.putFiles([{ local: entity.lc, remote: entity.rm }]).then(function () {
+            console.log(entity, "The File thing is done")
+        }, function (error) {
+            console.log(entity, "Something's wrong")
+            console.log(error)
+        });
+    }
+    console.log('Finish');
+
+    return;
+
     const failed = [];
     const successful = [];
     ssh.putDirectory(p, '/home/TEST', {

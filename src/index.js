ff934f0ccaf7b07ee748eb30e2c650cc0e4878e1
Jodu555 <Jodu505@gmail.com>
2022-02-04 12:44:56 +0100

    Moved the deploys out of the index

--- src/index.js
+++ src/index.js
@@ -16,28 +16,10 @@ if (!fs.existsSync('deployments'))
 if (!fs.existsSync('history'))
     fs.mkdirSync('history');
 
-const { setupConfig, registerDeploy } = require('./utils/utils');
+const { setupConfig } = require('./utils/utils');
 setupConfig();
 
-registerDeploy('Personal-Website', {
-    steps: ['Download', 'Deletion', 'Upload'],
-    gh_repo_URL: 'https://github.com/Jodu555/Personal-Website',
-    gh_repo_wh_SECRET: 'TEST123'
-},
-    async (deploy, host, data, config) => {
-        deploy.createDeploy();
-        deploy.exec(`git clone https://github.com/Jodu555/Personal-Website .`);
-        deploy.step();
-        deploy.delete('README.md');
-        deploy.delete(['.git', '.gitignore', '.vscode']);
-        deploy.step();
-
-        await host.connect(config.get('ci'), '/var/www/JODU');
-        await host.upload();
-        host.disconnect();
-
-        deploy.deleteDeploy();
-    });
+require('./utils/deploys');
 
 const app = express();
 app.use(cors());
---
+++ src/utils/deploys.js
@@ -0,0 +1,21 @@
+const { registerDeploy } = require('./utils/utils');
+
+registerDeploy('Personal-Website', {
+    steps: ['Download', 'Deletion', 'Upload'],
+    gh_repo_URL: 'https://github.com/Jodu555/Personal-Website',
+    gh_repo_wh_SECRET: 'TEST123'
+},
+    async (deploy, host, data, config) => {
+        deploy.createDeploy();
+        deploy.exec(`git clone https://github.com/Jodu555/Personal-Website .`);
+        deploy.step();
+        deploy.delete('README.md');
+        deploy.delete(['.git', '.gitignore', '.vscode']);
+        deploy.step();
+
+        await host.connect(config.get('ci'), '/var/www/JODU');
+        await host.upload();
+        host.disconnect();
+
+        deploy.deleteDeploy();
+    });

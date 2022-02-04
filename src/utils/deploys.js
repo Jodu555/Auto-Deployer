1088c071d7bff7160acf824b48274e5048d39969
Jodu555 <Jodu505@gmail.com>
2022-02-04 12:49:20 +0100

    Started to build some of the deploys more out

--- src/utils/deploys.js
+++ src/utils/deploys.js
@@ -3,7 +3,7 @@ const { registerDeploy } = require('./utils/utils');
 registerDeploy('Personal-Website', {
     steps: ['Download', 'Deletion', 'Upload'],
     gh_repo_URL: 'https://github.com/Jodu555/Personal-Website',
-    gh_repo_wh_SECRET: 'TEST123'
+    gh_repo_SECRET: 'TEST123'
 },
     async (deploy, host, data, config) => {
         deploy.createDeploy();
@@ -13,7 +13,7 @@ registerDeploy('Personal-Website', {
         deploy.delete(['.git', '.gitignore', '.vscode']);
         deploy.step();
 
-        await host.connect(config.get('ci'), '/var/www/JODU');
+        await host.connect(config.get('root'), '/var/www/html/main');
         await host.upload();
         host.disconnect();
 

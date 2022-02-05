dafdd15b2fd327bab593e7bf54ff7d954ce71da4
Jodu555 <Jodu505@gmail.com>
2022-02-05 10:54:36 +0100

    added an idea where the webhooks can be implemented

--- src/utils/deploys.js
+++ src/utils/deploys.js
@@ -3,7 +3,8 @@ const { registerDeploy } = require('./utils');
 registerDeploy('Personal-Website', {
     steps: ['Download', 'Deletion', 'Upload'],
     gh_repo_URL: 'https://github.com/Jodu555/Personal-Website',
-    gh_repo_SECRET: 'TEST123'
+    gh_repo_SECRET: 'TEST123',
+    webhooks: ['test'],
 },
     async (deploy, host, data, config) => {
         deploy.createDeploy();

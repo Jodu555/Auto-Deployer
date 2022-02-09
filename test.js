dba1486efc201dbed53f84e626c29ec77152102e
Jodu555 <Jodu505@gmail.com>
2022-02-09 13:15:48 +0100

    Brought the test/dream code back to try to fix the error

---
+++ test.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { exec } = require("child_process");
+
+const dir = path.join(process.cwd(), 'deployments', '#EZ-Uploader - Deployment');
+
+
+async function executeCommand(command, cwd) {
+    return await new Promise((resolve, reject) => {
+        exec(command, { encoding: 'utf8', cwd }, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            }
+            if (stderr) {
+                reject(stderr);
+            }
+            resolve(stdout)
+        });
+    })
+}
+
+async function run() {
+    const out = await executeCommand('npm i', dir);
+
+    console.log(`out: `, out);
+
+}
+
+run();
+
+// const command = child_process.spawnSync('npm', ['i'], { encoding: 'utf8', cwd: dir });
+
+// console.log(command);
